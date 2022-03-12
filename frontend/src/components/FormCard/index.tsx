8ab3d677c9b39fc2dcc3d682a4b9271f3044aeed
Gustavo <gustavo100palmeirascosta@hotmail.com>
2022-03-12 00:31:02 -0300

    finalizing integration

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link , useNavigate } from 'react-router-dom';
-import { Movie } from 'types/movie';
-import axios, { AxiosRequestConfig } from 'axios'
+import { Movie } from "types/movie";
+import axios, { AxiosRequestConfig } from 'axios';
 import { BASE_URL } from 'utils/requests';
 import { validateEmail } from 'utils/validate';
 import './styles.css';
